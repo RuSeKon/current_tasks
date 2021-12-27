42a43a451a37692b20421b3bca1ab4ed2e74180e
RuSeKon <ruskon1998 at gmail dot com>
2021-12-27 10:26:50 +0300

    Modified hash_table.c as full define processor

--- hash_table.c
+++ hash_table.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
 
 #define HASHSIZE 101
 #define MAXNAME 100
 #define MAXDEFN 100
 #define ISDEF(c) (c == '#' && (c = getchar() == 'd'))
 #define DEF 0
+#define SPACE 32
 
 struct nlist {
 	struct nlist *next;
@@ -17,32 +18,48 @@ struct nlist {
 
 static struct nlist *hashtab[HASHSIZE];
 
-int getchar(FILE *, char *, char *, int, int);
+int getchr(FILE *, FILE *, char *, char *, int, int);
 struct nlist *lookup(char *, int);
+struct nlist *install(char *, char *);
 
 main(int argc, char **argv)
-{	char tmp_name[MAXNAME];
-	char tmp_defn[MAXDEFN];
+{
+	char name[MAXNAME];
+	char defn[MAXDEFN];
 	int res;
-	FILE *fp;
+	FILE *inp_f, *outp_f;
 	struct nlist *np;
 
-	if(argc < 2)
+	if(argc < 2) {
 		puts("Please, enter filename!\n");
+		return 1;
+	}
+	inp_f = fopen(argv[1], "r");
 
-	fp = fopen(argv[1], "r+");
+	if(argc > 2 && isalpha(argv[2][0])) //name for processed file
+		outp_f = fopen(argv[2], "w");
+	else
+		outp_f = fopen("processed.c", "w");
 
-	while((res = getchar(fp, tmp_name, tmp_defn, MAXNAME, MAXDEFN)) != EOF)
-	    if(res == DEF)
-	        if(install(tmp_name, tmp_defn, 0) == NULL) {
+	while((res = getchr(outp_f, inp_f, name, defn, MAXNAME, MAXDEFN)) != EOF)
+    	    if(res == DEF) {
+	        if(install(name, defn) == NULL) {
 		    printf("Error of install definition on table\n");
 		    return 1;
 		}
-	    else 
-		if((np = lookup(tmp_name, 0)) == NULL)
+	    } else { 		//getchar returned name
+		if((np = lookup(name, 0)) == NULL) { //if name is not in hashtable 
+			fputs(name, outp_f);
+			fputc(res, outp_f);	//put name to outfile without cast
 			continue;
-		lseek(fp, -res, SEEK_CUR);
-		put(fp, np->name);
+		}
+	   fputs(np->defn, outp_f);	//if name is in table put define val
+	   fputc(res, outp_f);
+	   }
+	fclose(inp_f);
+	fclose(outp_f);
+	return 0;
+}
 
 
 
@@ -54,6 +71,8 @@ unsigned hash(char *s)
 		hashval = *s + 31 *hashval;
 	return hashval % HASHSIZE;
 }
+
+int del_node(char *);
 
 struct nlist *lookup(char *s, int del)
 {
@@ -70,12 +89,12 @@ struct nlist *lookup(char *s, int del)
 }
 
 
-struct nlist *install(char *name, char *defn, int del)
+struct nlist *install(char *name, char *defn)
 {
 	struct nlist *np;
 	unsigned hashval;
 
-	if((np = lookup(name, del)) == NULL) {
+	if((np = lookup(name, 0)) == NULL) {
 		np = (struct nlist *)malloc(sizeof(*np));
 		if(np == NULL || (np->name = strdup(name)) == NULL)
 			return NULL;
@@ -94,58 +113,53 @@ int del_node(char *nm)
     struct nlist *np, *tmp;
 
     /* search node with field.name = nm in hashtable */
-    for(np = hashtable[hash(nm)]; np != NULL; np = np->next) { 
-	if(np->next == NULL && (strcmp(np->name, nm) == 0)){ //if np is ones or last 
-	    free((void*)np);			    //in list, and .name = nm
+    for(np = hashtab[hash(nm)]; np != NULL; np = np->next) { 
+	if(np->next == NULL && 
+			(strcmp(np->name, nm) == 0)){ //if np is ones or last 
+	    free((void*)np);			      //in list, and .name = nm
 	    return 0;
-	} else if(strcmp(np->next->name, nm) == 0) { 	    //if .name = nm,
-	    tmp = np->next;				    //and np not last or ones
+	} else if(strcmp(np->next->name, nm) == 0) { //if .name = nm,
+	    tmp = np->next;			     //and np not last or ones
 	    np->next = tmp->next ? tmp->next : NULL;
 	    return 0;
 	} else 
 	    return 1;	//nm is missing in hashtable	
 	}
+    return -1;
 }
 			
-int getchar(FILE *fp, char *name, char *defn, int max_nm, int max_def)
+int getchr(FILE *out, FILE *in,  char *nm, char *dfn, int max_nm, int max_dfn)
 {
     int i;
-    while((c = fgetc(fp)) != EOF) {
-
-        if(//ISDEF(c)) {
-	    while((c = fgetc(fp)) != ' ') //skip word /define/
-		;
-
-	    for(i = 0; i < max_nm && (c = getchar()) != ' '; i++)
-		name[i] = c;
-	    if(i >= max) //too much longer define name
-		return EOF;
-
-	    for(i = 0; i < max_def && (c = fgetc(fp)) != ' '
-			    && c != '\n'; i++) //only one line define
-		defn[i] = c; 
-	    if(i >= max) //too much longer definition
-		return EOF;
-	return DEF;
-	}
-
-	if(isalpha(c)) {
-	    for(i = 0;i < max_nm &&  
-		(c = fgetc(fp)) != ' ' && c !=  '\n'; i++) {
-		    if(isalpha(c))
-			name[i] = c;
-		    else if(c == '_')
-			name[i] = c;
-		    else {
-			name[i] = '\0';
-			return i;
-		    }
-	    }
-	    name[i] = '\0';
-	    return i;
-	}
+    char c;
+    while((c = fgetc(in)) != EOF) {
+	if(c == '#') {
+	     fputc(c, out);
+	     while((c = fgetc(in)) != SPACE)
+		fputc(c, out);
+	     fputc(c, out);
+	     for(i = 0; i < max_nm && (c=fgetc(in)) != SPACE; i++) {
+		     fputc(c, out);
+		     nm[i] = c;
+	     }
+	     fputc(c, out);
+	     nm[i] = '\0';
+	     for(i = 0; i < max_dfn && 
+			     (c=fgetc(in)) != SPACE && c != '\n'; i++) {
+		     fputc(c, out);
+		     dfn[i] = c;
+	     }
+	     fputc(c, out);
+	     dfn[i] = '\0';
+	     return DEF;
+	} else if(isalpha(c)) {
+	     nm[0] = c;
+	     for(i = 1; i < max_nm && (isalpha(c=fgetc(in)) || c == '_'); i++)
+		nm[i] = c;
+	     nm[i] = '\0';
+	     return c;
+	} else
+	     fputc(c, out);
     }
     return EOF;
 }
-
-void put(
